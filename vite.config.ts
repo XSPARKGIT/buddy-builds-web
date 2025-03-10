e969f27543ea15006f6480d9a0fb6a97ab7535dd
unknown <xsparkatx@gmail.com>
2025-03-10 20:33:34 +0200

    deploy test8

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/buddy-builds-web/",
+  plugins: [react()],
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
+    }
   },
-}));
+  server: {
+    port: 3000,
+    open: true
+  }
+});
